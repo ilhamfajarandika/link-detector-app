bce8ce82b4cb163a120ffa69a7f66e6c4fc79503
Ilham Fajar Andika <ilhamfajar317@gmail.com>
2026-03-12 21:20:21 +0700

    update mobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,27 +8,23 @@ function App() {
   const [result, setResult] = useState(null);
 
   const checkUrl = async (url) => {
+    // const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/detect`, {
+    //   url,
+    // });
+    // const res = await axios.post("http://localhost:5000/api/detect", { url });
     const res = await axios.post("/api/detect", { url });
 
     setResult(res.data);
-    // console.log(res.data);
+    console.log(res.data);
   };
 
   return (
-    <div className="min-h-screen bg-slate-950 text-white flex flex-col items-center px-4 py-10">
-      <div className="w-full max-w-xl flex flex-col items-center">
-        <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center">
-          Link Guardian
-        </h1>
+    <div className="min-h-screen bg-slate-950 text-white flex flex-col items-center p-10">
+      <h1 className="text-3xl font-bold mb-8">Link Guardian</h1>
 
-        <UrlInput onCheck={checkUrl} />
+      <UrlInput onCheck={checkUrl} />
 
-        {result && (
-          <div className="w-full mt-6">
-            <ResultCard result={result} />
-          </div>
-        )}
-      </div>
+      {result && <ResultCard result={result} />}
     </div>
   );
 }
--- client/src/components/UrlInput.jsx
+++ client/src/components/UrlInput.jsx
@@ -5,19 +5,29 @@ function UrlInput({ onCheck }) {
   const [url, setUrl] = useState("");
 
   return (
-    <div className="flex gap-2">
+    <div className="flex flex-col sm:flex-row gap-2 w-full">
       <input
         value={url}
         onChange={(e) => setUrl(e.target.value)}
         placeholder="Paste suspicious URL..."
-        className="bg-slate-800 px-4 py-3 rounded-lg w-[420px]"
+        className="bg-slate-800 px-4 py-3 rounded-lg w-full"
       />
 
-      <button
+      {/* <button
         onClick={() => onCheck(url)}
-        className="bg-blue-600 px-4 rounded-lg flex items-center gap-2"
+        className="bg-blue-600 px-4 py-3 rounded-lg flex items-center justify-center gap-2 sm:w-auto w-full"
       >
         <Search size={18} />
+        Scan
+      </button> */}
+
+      <button
+        onClick={() => {
+          console.log("clicked");
+          onCheck(url);
+        }}
+        className="bg-blue-600 px-4 py-3 rounded-lg flex items-center justify-center gap-2 sm:w-auto w-full"
+      >
         Scan
       </button>
     </div>
