ddb151b416c7ce8c50d6c555b108d614e5570eea
Ilham Fajar Andika <ilhamfajar317@gmail.com>
2026-03-13 17:00:45 +0700

    update hyperlink dan favicon

--- api/detect.js
+++ api/detect.js
@@ -12,37 +12,27 @@
 //   res.status(200).json(result);
 // }
 
-export default function handler(req, res) {
-  res.status(200).json({ message: "API is working" });
-}
-
-// export default async function handler(req, res) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ message: "Method not allowed" });
-//   }
-
-//   const { url } = req.body;
-
-//   if (!url) {
-//     return res.status(400).json({ message: "URL is required" });
-//   }
-
-//   try {
-//     const suspiciousWords = ["login", "verify", "bank", "update", "secure"];
-
-//     const found = suspiciousWords.some(word =>
-//       url.toLowerCase().includes(word)
-//     );
-
-//     res.status(200).json({
-//       url,
-//       isSuspicious: found,
-//       message: found
-//         ? "This link looks suspicious"
-//         : "This link looks safe",
-//     });
-
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error" });
-//   }
+// export default function handler(req, res) {
+//   res.status(200).json({ message: "API is working" });
 // }
+
+import detectLink from "../utils/detectLink.js";
+
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+
+  try {
+    const result = detectLink(url);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+}
